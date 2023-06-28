14747bdc1fe2e6cbd4b5ecb790cb4795cebcc94d
laura.baluchova <laura.menova@gmail.com>
2023-06-28 10:42:17 +0200

    formatted

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,4 @@
-const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 
 const stub = ClarifaiStub.grpc();
 
@@ -6,42 +6,46 @@ const metadata = new grpc.Metadata();
 metadata.set("authorization", process.env.API_KEY);
 
 const handleApiCall = (req, res) => {
+  stub.PostModelOutputs(
+    {
+      model_id: req.body.module.id,
+      inputs: [{ data: { image: { url: req.body.input } } }],
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        console.log("Error: " + err);
+        return;
+      }
 
-    stub.PostModelOutputs(
-        {
-            
-            model_id: req.body.module.id,
-            inputs: [{data: {image: {url: req.body.input}}}]
-        },
-        metadata,
-        (err, response) => {
-            if (err) {
-                console.log("Error: " + err);
-                return;
-            }
-    
-            if (response.status.code !== 10000) {
-                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-                return;
-            }    
+      if (response.status.code !== 10000) {
+        console.log(
+          "Received failed status: " +
+            response.status.description +
+            "\n" +
+            response.status.details
+        );
+        return;
+      }
 
-            res.json(response)
-        }
-    );
-}
+      res.json(response);
+    }
+  );
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
   handleImage,
-  handleApiCall
-}
+  handleApiCall,
+};
